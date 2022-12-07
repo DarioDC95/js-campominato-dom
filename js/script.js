69f175e1efdf363d972ccca08e3d3ebbb62536a1
DarioDC95 <dario_95@live.com>
2022-12-07 17:03:59 +0100

    Implemented the condition for victory or defeat

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
+// Funzione per la creazione di quadrati
 function createSquare(ind) {
     const squareDiv = document.createElement('div');
     squareDiv.className = 'square';
     squareDiv.innerText = ind;
     return squareDiv;
+}
+
+// Funzione per la creazione dell'array con numeri casuali
+function arrayRandomNumber(min, max) {
+    let array = [];
+    let i = 0;
+    while (i < 16) {
+        let num = Math.floor(Math.random() * (max - min + 1) + 1);
+        if (!array.includes(num)) {
+            array.push(num);
+            i++
+        }
+    }
+    return array;
 }
 
 // Definiamo il pulsante di play
@@ -29,20 +44,53 @@ button_play.addEventListener('click', function(){
         n_celle = 49;
     }
 
+    // creo un arrey di bombe
+    let bombs = arrayRandomNumber(1, n_celle);
+    console.log(bombs);
+
     // richiamo la funzione e incollo i div nel contenitore
     for (let i = 1; i <= n_celle; i++) {
         divSquare = createSquare(i);
 
-        // aggiungere il background azzurro quando clicco sul quadrato
+        // Aggiungo il addEventListener al quadrato
         divSquare.addEventListener('click', function(){
-            this.classList.toggle('lightblue');
-            console.log(this.innerText);
+
+            // aggiungo la possibilità del quadrato bomba e li coloro tutti di rosso se premuto
+            if (bombs.includes(parseInt(this.innerText))) {
+                for (let i = 0; i < n_celle; i++) {
+                    squareArray[i].classList.add('pointer-event-none');
+                    if (bombs.includes(parseInt(squareArray[i].innerText))) {
+                        squareArray[i].classList.add('red');
+                    }
+                }
+            }
+
+            else {
+                // aggiungere il background azzurro quando clicco sul quadrato non bomba
+                this.classList.toggle('lightblue');
+                console.log(this.innerText);
+
+                // creo un array di non bombe
+                let squareArrayNotBombs = [];
+
+                for (let i = 0; i < n_celle; i++) {
+                    if (squareArray[i].classList.contains('lightblue')) {
+                        squareArrayNotBombs.push(squareArray[i]);
+                    }
+                }
+                console.log(squareArrayNotBombs)
+
+                if (squareArrayNotBombs.length == (n_celle - 16)){
+                    for (let i = 0; i < n_celle; i++) {
+                        squareArray[i].classList.add('pointer-event-none');
+                    }
+                }
+                
+            }
+
         })
 
-        // incollo il quadrato nel suo container
-        containerSquare.append(divSquare);
-
-        // aggiungere la classe con la dimensione giusta in base alla dificoltà
+        // aggiungere la classe con la dimensione giusta in base alla difficoltà
         if (difficultySelected === 'easy') {
             divSquare.classList.add('hard-size');
         }
@@ -52,6 +100,13 @@ button_play.addEventListener('click', function(){
         else {
             divSquare.classList.add('easy-size');
         }
+
+        // incollo il quadrato nel suo container
+        containerSquare.append(divSquare);
     }
+
+    // creo un array con tutte le classi square
+    let squareArray = document.getElementsByClassName('square');
+    console.log(squareArray);
 
 })
